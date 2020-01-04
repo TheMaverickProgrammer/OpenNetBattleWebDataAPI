7f0fd5b37a125c5582137a3f95a48fb8d6c113f1
TheMaverickProgrammer <bmpeppers@gmail.com>
2020-01-04 01:42:22 -0600

    builds. tables are formatted with color and parses json from http requests

--- client/ConsoleApplication.cpp
+++ client/ConsoleApplication.cpp
@@ -1,13 +1,16 @@
 // ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-//#include <tabulate/table.hpp>
+#include <cctype>
+#include <winsock2.h>
+#include <tabulate/table.hpp>
 #include <httplib/httplib.h>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <algorithm>
 #include <iterator>
+#include <json/json.h>
+#include <string>
 
 #define MAKE_API_URI(x) (std::string("/v1/") + x)
 
@@ -44,9 +47,19 @@ void login(httplib::Client& client) {
 }
 
 void help() {
-  std::cout << "commands are: view <RESOURCES>/:ID?, delete <RESOURCES>/:ID?, add <RESOURCES> <BODY>, update <RESOURCES>/:ID, login" << std::endl;
-  std::cout << "resources are: users, chips, folders, public-folders, admins" << std::endl;
-  std::cout << "type QUIT to quit" << std::endl;
+  tabulate::Table table;
+  table.add_row({ "command", "description", "example" });
+  table.format().hide_border().font_color(tabulate::Color::yellow);
+
+  table.add_row({ "view", "list all entries. If ID is given, list the contents of that ID.", "view users 19" });
+  table.add_row({ "delete", "ID required. Permanently deletes a resource", "delete folder 143" });
+  table.add_row({ "add", "adds an entry to a resource", "add chip ..." });
+  table.add_row({ "update", "ID required. Permanently alters a resource", "update folder 143 ..." });
+  table.add_row({ "login", "Prompts for account username, pass, and then saves the auth", "login" });
+  table.add_row({ "quit", "Exits and any login information is lost", "quit" });
+
+  std::cout << table << std::endl;
+
 }
 void handleGetRequest(httplib::Client& client, std::vector<std::string>& input) {
 
@@ -67,16 +80,50 @@ void handleDeleteRequest(httplib::Client& client, std::vector<std::string>& inpu
 void handleViewRequest(httplib::Client& client, std::string& input) {
   input = MAKE_API_URI(input);
 
-  std::cout << "Making request to " << input << "\"" << std::endl;
+  std::cout << "Making request to \"" << input << "\"" << std::endl;
 
   auto res = client.Get(input.c_str());
 
+  tabulate::Table table;
+  auto& f = table.add_row({ "ID", "data" }).format();
+  f.background_color(tabulate::Color::blue);
+  f.font_color(tabulate::Color::white);
+  f.hide_border();
+
   if (res) {
-    std::cout << res->status << " -> " << res->body << std::endl;
+
+    Json::CharReaderBuilder builder;
+    Json::CharReader* reader = builder.newCharReader();
+
+    Json::Value json;
+    std::string errors;
+
+    bool parsingSuccessful = reader->parse(
+      res->body.c_str(),
+      res->body.c_str() + res->body.size(),
+      &json,
+      &errors
+    );
+    delete reader;
+
+    if (!parsingSuccessful)
+    {
+      throw std::exception("could not read json");
+    }
+    
+    const Json::Value arr = json["data"];
+
+    for (int i = 0; i < arr.size(); i++) {
+      const auto item = arr[i];
+
+      table.add_row({ item["_id"].asCString(), item["code"].asCString() });
+    }
   }
   else {
     throw std::runtime_error("http result was nullptr");
   }
+
+  std::cout << table << std::endl;
 }
 
 void handleCommands(httplib::Client& client, std::string& input) {
@@ -121,7 +168,13 @@ void handleCommands(httplib::Client& client, std::string& input) {
     }
   }
   catch (std::exception e) {
-    std::cout << "There was an error making the request: " << e.what() << std::endl;;
+    tabulate::Table table;
+    auto& f = table.add_row({ "status", "body" }).format();
+    table.add_row({ "internal error", e.what() });
+    f.background_color(tabulate::Color::red);
+    f.font_color(tabulate::Color::white);
+
+    std::cout << table  << std::endl;;
   }
 }
 
