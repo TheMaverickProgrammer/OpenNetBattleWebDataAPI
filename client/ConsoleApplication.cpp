8d2ffd3ee871c058102e0825ca5f8fad799b75eb
TheMaverickProgrammer <bmpeppers@gmail.com>
2020-01-05 17:20:53 -0600

    able to add data from the client. Still cannot download or upload files however

--- client/ConsoleApplication.cpp
+++ client/ConsoleApplication.cpp
@@ -27,6 +27,32 @@ std::vector<std::string> split(const std::string& str, const char delim = ' ')
   return cont;
 }
 
+std::vector<std::string> split_preserve_quotes(const std::string& str, const char delim = ' ') {
+  auto cont = std::vector<std::string>();
+
+  std::string build;
+  bool quoted = false;
+
+  for (auto iter = str.begin(); iter != str.end();) {
+    if (*iter == delim && !quoted) {
+      cont.push_back(build);
+      build.clear();
+      iter++;  continue;
+    }
+    else if (*iter == '\"') {
+      quoted = !quoted;
+    }
+
+    build += *iter;
+    iter++;
+  }
+
+  if (build.size()) {
+    cont.push_back(build);
+  }
+  return cont;
+}
+
 #if defined(_MSC_VER) && _MSC_VER > 1310
 // Visual C++ 2005 and later require the source files in UTF-8, and all strings 
 // to be encoded as wchar_t otherwise the strings will be converted into the 
@@ -186,7 +212,7 @@ void displayChipsResource(const Json::Value& data, bool detail = false) {
     f.font_color(tabulate::Color::white);
     f.hide_border();
 
-    for (int i = 0; i < data.size(); i++) {
+    for (Json::ArrayIndex i = 0; i < data.size(); i++) {
       const auto item = data[i];
 
       table.add_row({ item["_id"].asCString(), item["code"].asCString() });
@@ -196,12 +222,112 @@ void displayChipsResource(const Json::Value& data, bool detail = false) {
   std::cout << table << std::endl;
 }
 
-void handleGetRequest(httplib::Client& client, std::vector<std::string>& input) {
-
-}
-
 void handlePostRequest(httplib::Client& client, std::vector<std::string>& input) {
+  std::string uri;
+  std::string resource = input[0];
 
+  if (!(
+    matches(resource, "chips")
+    || matches(resource, "users")
+    || matches(resource, "folders")
+    || matches(resource, "public-folders")
+    || matches(resource, "admin")
+    )) {
+    std::string message = (std::string("unknown resource ") + resource);
+    message += "\nType `help resources` for a list of resources";
+    throw std::exception(message.c_str());
+  }
+
+  uri = MAKE_API_URI(resource);
+
+  //uri += '?';
+
+  std::string params;
+
+  for (int i = 1; i < input.size(); i++) {
+    auto tokens = split(input[i], '=');
+
+    if (tokens.size() != 2) {
+      std::string message = "Token " + input[i] + " did not contain a key-value pair separated by \"=\"\nRequest aborted.";
+      throw std::exception(message.c_str());
+    }
+
+    params += input[i] + "&";
+  }
+
+  std::cout << "Making request to \"" << uri << "\"";
+
+  auto res = client.Post(uri.c_str(), params, "application/x-www-form-urlencoded");
+
+  if (res) {
+
+    Json::CharReaderBuilder builder;
+    Json::CharReader* reader = builder.newCharReader();
+
+    Json::Value json;
+    std::string errors;
+
+    bool parsingSuccessful = reader->parse(
+      res->body.c_str(),
+      res->body.c_str() + res->body.size(),
+      &json,
+      &errors
+    );
+    delete reader;
+
+    if (!parsingSuccessful)
+    {
+      std::cout << std::endl;
+      throw std::exception("could not read json");
+    }
+
+    const Json::Value error = json["error"];
+
+    if (error.size()) {
+      std::cout << std::endl;
+      std::string message;
+      bool isChain = false; // whether or not we need to accumulate all messages;
+
+      auto members = error.getMemberNames();
+      for (auto& m : members) {
+        if (m == "errors") {
+          isChain = true;
+        }
+      }
+
+      if (isChain) {
+        for (auto& m : error["errors"]) {
+          message += m["message"].asString() + "\n";
+        }
+      }
+      else {
+        message = error["message"].asString();
+      }
+
+      std::cout << std::endl;
+      throw std::exception(message.c_str());
+    }
+
+    const Json::Value arr = json["data"];
+    tabulate::Table results;
+    int size = arr.size();
+
+    if (size > 0) size -= 1; // json value returns +1 results for the table header
+    auto& f = results.add_row({ std::to_string(size) + " results" }).format();
+    f.background_color(tabulate::Color::blue);
+    f.font_color(tabulate::Color::white);
+    f.hide_border();
+
+    std::cout << "..." << results << std::endl;
+
+    if (resource == "chips") {
+      displayChipsResource(arr, input.size() != 1);
+    }
+  }
+  else {
+    std::cout << std::endl;
+    throw std::runtime_error("http result was nullptr");
+  }
 }
 
 void handlePutRequest(httplib::Client& client, std::vector<std::string>& input) {
@@ -298,7 +424,7 @@ void handleCommands(httplib::Client& client, std::string& input) {
     return;
   }
 
-  auto tokens = split(input);
+  auto tokens = split_preserve_quotes(input);
 
   if (tokens.size() < 2) {
     std::cout << "type 'help' for commands" << std::endl;
